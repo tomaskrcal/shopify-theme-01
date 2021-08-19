0b571d78b190a33e48b4f0bc2fcbc8810c26295a
tomaskrcal <krcaltomas99@gmail.com>
2021-08-20 01:10:05 +0200

    Přidání homepage headeru

---
+++ assets/application.js
@@ -0,0 +1,6 @@
+// Put your applicaiton javascript here
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image, {
+  maxTransition: 250,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: [
+      './layout/*.liquid',
+      './templates/*.liquid',
+      './sections/*.liquid',
+      './snippets/*.liquid',
+    ],
+  },
+  corePlugins: {
+    // ...
+    fontFamily: false,
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
